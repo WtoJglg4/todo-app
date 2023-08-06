2481eaa58b3a14df99bf048c961222a7b055133d
WtoJglg4 <glazovv81@gmail.com>
2023-08-06 18:16:59 +0300

    The mechanism of registration and authentication of users is implemented. JWT implementation.

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,9 +5,21 @@ import (
 	"fmt"
 	"github/todo-app"
 	"github/todo-app/pkg/repository"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
-const salt = "gsafdkkghkjcsafgjsdgdkfa"
+const (
+	salt       = "gsafdkkghkjcsafgjsdgdkfa"
+	tokenTTL   = 12 * time.Hour
+	signingKey = "hsH&by1!21uAG#AGHuas_7qw8"
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -20,6 +32,23 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
